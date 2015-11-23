30aef37c08b2f72790e02132c96706379bf01fcc
Luke Boyle <luke.b@checkerplatesoftware.com.au>
2015-11-23 14:17:31 +1100

    Adds the transformToStorage and transformFromStorage functions

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
+// http://stackoverflow.com/questions/16427636/check-if-localstorage-is-available
+
+function isLocalStorageAvailable(){
+    var test = 'test';
+    try {
+        localStorage.setItem(test, test);
+        localStorage.removeItem(test);
+        return true;
+    } catch(e) {
+        return false;
+    }
+}
+
+var errorMessage = 'Warning, local storage is not available in your current environment. This module does not work without local storage available';
+
 /**
  *
  * @param {String} key - the local storage key
  * @param {String || Number || Array || Object} data - the data to enter into the key
  */
 function put(key, data) {
-    localStorage[key] = JSON.stringify(data);
+    if (isLocalStorageAvailable() === true) {
+        localStorage[key] = JSON.stringify(data);
+    } else {
+        throw new Error(errorMessage)
+    }
 }
 
 /**
@@ -12,7 +31,15 @@ function put(key, data) {
  * @param {String} key - fetches all data in the key and de-stringifies it
  */
 function fetch(key) {
-    return JSON.parse(localStorage[key]);
+    if (isLocalStorageAvailable() === true) {
+        if (localStorage[key] === undefined) {
+            return undefined;
+        } else {
+            return JSON.parse(localStorage[key]);
+        }
+    } else {
+        throw new Error(errorMessage)
+    }
 }
 
 /**
@@ -20,9 +47,48 @@ function fetch(key) {
  * @param {String} key - the key to remove and delete all data in
  */
 function remove(key) {
-    return localStorage.removeItem(key);
+    if (isLocalStorageAvailable() === true) {
+        return localStorage.removeItem(key);
+    } else {
+        throw new Error(errorMessage)
+    }
+}
+
+/**
+ *
+ * @param {String} str - The string to process
+ * @param {String} find - The characters to find
+ * @param {String} replace - The characters to replace the found characters with
+ * @returns {string|XML|void}
+ */
+function replaceAll(str, find, replace) {
+    return str.replace(new RegExp(find, 'g'), replace);
+}
+
+/**
+ *
+ * @param {String} string - The string to process
+ * @param {String} find - The characters to find
+ * @param {String} replace - The characters to replace the found characters with
+ * @returns {string|XML|void}
+ */
+function transformToStorage(string, find, replace) {
+    return replaceAll(string, find, replace);
+}
+
+/**
+ *
+ * @param {String} string - The string to process
+ * @param {String} find - The characters to find
+ * @param {String} replace - The characters to replace the found characters with
+ * @returns {*} - Does the opposite of {@link transformToStorage}
+ */
+function transformFromStorage(string, find, replace) {
+    return replaceAll(string, find, replace);
 }
 
 module.exports.set = put;
 module.exports.get = fetch;
 module.exports.remove = remove;
+module.exports.transformToStorage = transformToStorage;
+module.exports.transformFromStorage = transformFromStorage;
