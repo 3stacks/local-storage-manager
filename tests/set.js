cd063649408e434bd3c5d7df675a8e7d22af339a
Luke Boyle <luke.boyle.au@gmail.com>
2016-10-13 09:00:04 +1100

    test: add the basics for set

--- tests/set.js
+++ tests/set.js
@@ -3,4 +3,26 @@ import { set } from '../src/index';
 
 describe('Set function', () => {
 
+    describe('The basics', () => {
+
+        it('should be available', () => {
+
+            expect(
+                set
+            ).toExist();
+
+        });
+
+        it('should be a function', () => {
+
+            expect(
+                set
+            ).toBeA(
+                Function
+            )
+
+        })
+
+    });
+
 });
