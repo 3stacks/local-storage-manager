e1e7001924cd765a354fa5c6a1548426ad4e0e8a
Luke Boyle <luke.boyle.au@gmail.com>
2016-10-13 12:35:16 +1100

    chore: add a conditional test process for travis

--- karma.config.js
+++ karma.config.js
@@ -1,43 +1,56 @@
 module.exports = function(config) {
-	config.set({
-		files: [
-			{ pattern: 'tests/**/**/**.*', watched: true }
-		],
-		preprocessors: {
-			'tests/**/**.*': ['webpack'],
-			// Exclude the tests in the coverage report
-			'!**/**/**.test.js': ['coverage']
-		},
-		webpack: {
-			module: {
-				loaders: [
-					{
-						test: /\.js/,
-						exclude: /node_modules/,
-						loader: 'babel-loader',
-						query: {
-							"plugins": [
-								"istanbul",
-								"transform-runtime"
-							],
-							"presets" : ["es2015", "es2017"]
+	if (!process.env.TRAVIS) {
+		config.set({
+			files: [
+				{ pattern: 'tests/**/**/**.*', watched: true }
+			],
+			preprocessors: {
+				'tests/**/**.*': ['webpack'],
+				// Exclude the tests in the coverage report
+				'!**/**/**.test.js': ['coverage']
+			},
+			webpack: {
+				module: {
+					loaders: [
+						{
+							test: /\.js/,
+							exclude: /node_modules/,
+							loader: 'babel-loader',
+							query: {
+								"plugins": [
+									"istanbul",
+									"transform-runtime"
+								],
+								"presets" : ["es2015", "es2017"]
+							}
 						}
-					}
-				]
+					]
+				},
+				node: {
+					// Force webpack to replace fs with an empty package
+					fs: "empty"
+				},
+				watch: true
 			},
-			node: {
-				// Force webpack to replace fs with an empty package
-				fs: "empty"
+			webpackServer: {
+				noInfo: true
 			},
-			watch: true
-		},
-		webpackServer: {
-			noInfo: true
-		},
-		frameworks: ['mocha'],
-		reporters: ['mocha', 'coverage'],
-		browsers: ['Chrome'],
-		failOnEmptyTestSuite: true,
-		singleRun: true
-	})
+			frameworks: ['mocha'],
+			reporters: ['mocha', 'coverage'],
+			browsers: ['Chrome'],
+			failOnEmptyTestSuite: true,
+			singleRun: true
+		});
+	} else {
+		config.set(travisConfig);
+	}
+};
+
+var travisConfig = {
+	customLaunchers: {
+		Chrome_travis_ci: {
+			base: 'Chrome',
+			flags: ['--no-sandbox']
+		}
+	}
 };
