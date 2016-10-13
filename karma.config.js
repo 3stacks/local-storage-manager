0eebd63e3178d68ebf42c6b122f313e9b58782eb
Luke Boyle <luke.boyle.au@gmail.com>
2016-10-13 12:46:37 +1100

    chore: reorder the execution of the config

--- karma.config.js
+++ karma.config.js
@@ -1,57 +1,62 @@
-module.exports = function(config) {
-	if (!process.env.TRAVIS) {
-		config.set({
-			files: [
-				{ pattern: 'tests/**/**/**.*', watched: true }
-			],
-			preprocessors: {
-				'tests/**/**.*': ['webpack'],
-				// Exclude the tests in the coverage report
-				'!**/**/**.test.js': ['coverage']
-			},
-			webpack: {
-				module: {
-					loaders: [
-						{
-							test: /\.js/,
-							exclude: /node_modules/,
-							loader: 'babel-loader',
-							query: {
-								"plugins": [
-									"istanbul",
-									"transform-runtime"
-								],
-								"presets" : ["es2015", "es2017"]
-							}
-						}
-					]
-				},
-				node: {
-					// Force webpack to replace fs with an empty package
-					fs: "empty"
-				},
-				watch: true
-			},
-			webpackServer: {
-				noInfo: true
-			},
-			frameworks: ['mocha'],
-			reporters: ['mocha', 'coverage'],
-			browsers: ['Chrome'],
-			failOnEmptyTestSuite: true,
-			singleRun: true
-		});
-	} else {
-		config.set(travisConfig);
-	}
-};
-
-var travisConfig = {
+const configuration = {
+	files: [
+		{ pattern: 'tests/**/**/**.*', watched: true }
+	],
+	preprocessors: {
+		'tests/**/**.*': ['webpack'],
+		// Exclude the tests in the coverage report
+		'!**/**/**.test.js': ['coverage']
+	},
 	customLaunchers: {
 		Chrome_travis_ci: {
 			base: 'Chrome',
 			flags: ['--no-sandbox']
 		}
 	},
-	browsers: ['Chrome_travis_ci']
+	webpack: {
+		module: {
+			loaders: [
+				{
+					test: /\.js/,
+					exclude: /node_modules/,
+					loader: 'babel-loader',
+					query: {
+						"plugins": [
+							"istanbul",
+							"transform-runtime"
+						],
+						"presets" : ["es2015", "es2017"]
+					}
+				}
+			]
+		},
+		node: {
+			// Force webpack to replace fs with an empty package
+			fs: "empty"
+		},
+		watch: true
+	},
+	webpackServer: {
+		noInfo: true
+	},
+	frameworks: ['mocha'],
+	reporters: ['mocha', 'coverage'],
+	browsers: ['Chrome'],
+	failOnEmptyTestSuite: true,
+	singleRun: true
+};
+
+if (!process.env.TRAVIS) {
+	configuration.browsers = ['Chrome_travis_ci']
+}
+
+module.exports = function(config) {
+	if (!process.env.TRAVIS) {
+		config.set();
+	} else {
+		config.set({
+
+
+		});
+	}
 };
