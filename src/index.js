bfdf97f8ca265bcc795847352e72ac153f8ade73
Luke Boyle <luke.boyle.au@gmail.com>
2018-03-08 22:01:57 +1100

    refactor: remove deprecated function

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ import safeGet from 'lodash/get';
 import safeSet from 'lodash/set';
 // http://stackoverflow.com/questions/16427636/check-if-localstorage-is-available
 
-function isLocalStorageAvailable(){
-    var test = 'test';
+function isLocalStorageAvailable() {
+    const test = 'test';
+
     try {
         localStorage.setItem(test, test);
         localStorage.removeItem(test);
@@ -15,16 +16,9 @@ function isLocalStorageAvailable(){
 
 const localStorageError = 'Warning, local storage is not available in your current environment. This module does not work without local storage available';
 const undefinedError = 'Warning, the key or data is undefined. LocalStorage variables must not be undefined';
-const namespaceTypeError = new TypeError('argument `namespace` was expecting a string');
 
 function getPathArrayFromPath(path) {
 	return typeof path === 'string' ? path.split('/') : path;
-}
-
-function checkNamespaceType(namespaceInput) {
-    if (typeof namespaceInput !== 'string') {
-        throw namespaceTypeError;
-    }
 }
 
 /**
@@ -100,53 +94,3 @@ export function remove(path) {
 		};
 	}, rootData));
 }
-
-/**
- *
- * @param {String} str - The string to process
- * @param {String} find - The characters to find
- * @param {String} replace - The characters to replace the found characters with
- * @returns {string|XML|void}
- */
-function replaceAll(str, find, replace) {
-    return str.replace(new RegExp(find, 'g'), replace);
-}
-
-/**
- *
- * @param {String} string - The string to process
- * @param {String} find - The characters to find
- * @param {String} replace - The characters to replace the found characters with
- * @returns {string|XML|void}
- */
-export function transformToStorage(string, find, replace) {
-    return replaceAll(string, find, replace);
-}
-
-/**
- *
- * @param {String} string - The string to process
- * @param {String} find - The characters to find
- * @param {String} replace - The characters to replace the found characters with
- * @returns {*} - Does the opposite of {@link transformToStorage}
- */
-export function transformFromStorage(string, find, replace) {
-    return replaceAll(string, find, replace);
-}
-
-/**
- * @deprecated
- * @param {Object} defaultValues - Pass in an object with your application's local storage keys + their default value
- * @param {String} [namespace] - Optional parameter to add a namespace to scope your data to
- */
-export function setIfEmpty(defaultValues, namespace) {
-    Object.keys(defaultValues).forEach(function(key) {
-        let currentValue = defaultValues[key];
-        if (fetch(key) === undefined) {
-            put(key, currentValue, namespace)
-        }
-    });
-}
-
-export { put as set };
-export { fetch as get };
